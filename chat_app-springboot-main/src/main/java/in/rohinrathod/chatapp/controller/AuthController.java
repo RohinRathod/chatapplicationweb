f60b1fb6a06ea0317293843c739c5f94bec1e942
RJSA-22-10-2024 <RJSA-22-10-2024@DESKTOP-KHBA4NH>
2025-01-15 18:09:22 +0530

    changes in authController i.e @ModelAttribute

--- chat_app-springboot-main/src/main/java/in/rohinrathod/chatapp/controller/AuthController.java
+++ chat_app-springboot-main/src/main/java/in/rohinrathod/chatapp/controller/AuthController.java
@@ -3,9 +3,8 @@ package in.rohinrathod.chatapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.rohinrathod.chatapp.Entity.UserDto;
@@ -18,7 +17,7 @@ public class AuthController {
 	public UserService userService;
 
 	@PostMapping("/register")
-	public ResponseEntity<String> registerUser(UserDto userDto) {
+	public ResponseEntity<String> registerUser(@ModelAttribute UserDto userDto) {
 
 		userDto.setRole("ROLE_USER");
 		if (userService.findByUsername(userDto.getUsername())) {
